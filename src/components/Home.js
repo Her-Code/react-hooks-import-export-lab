0c91ad8e5a6542b7a7dc83790d3ab7f42a588304
+Sharon <+wanjiku.kahira@gmail.com>
2024-07-31 23:47:43 +0300

    Import and Export code to About,Home ,Navbar and User.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { username, city } from '../data/user';
 
 function Home() {
   return (
     <div id="home">
-      <h1>
-        {username} is a Web Developer from {city}
-      </h1>
+      <h1>Home</h1>
+      <p> Username: {username} is a Web Developer from City : {city}</p>
     </div>
   );
 }
+
+export default Home;
